0d6a39984640e10c2e201bcb731eb83f3b730599
AJ <ajthegreat12345x@gmail.com>
2023-12-23 13:35:02 +0800

    second push

---
+++ database/migrations/2023_12_22_164526_create_phonebook.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phonebook', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->integer('contact_number');
+            $table->boolean('is_deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('phonebook');
+    }
+};
